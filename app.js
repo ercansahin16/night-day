84b516212afa3adba897d5fb3383471c6dee932b
ercansahin16 <esahin.ast@gmail.com>
2024-10-11 23:28:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+
+// console.log(buton)
+// console.log(buton.id)
+// console.log(buton.className)
+
+
+// console.log(buton.classList)
+// const clastlistt=buton.classList
+// clastlistt.forEach((className)=>{
+//     console.log(className)
+// });
+// console.log(clastlistt[1])
+let changeText=()=>{
+    var buton=document.getElementById("butons")
+    var tema=document.getElementById("tema")
+    var icons=document.getElementById("icon")
+    var content=document.getElementById("content")
+
+    
+
+    if( icons.className==="fa-regular fa-sun"){
+        // Gündüz  modu
+        tema.classList.replace("bg-dark", "bg-light");
+        icons.className = "fa-regular fa-moon";
+        buton.classList.replace("btn-light", "btn-dark");
+        content.classList.replace("text-light", "text-dark");
+        
+    }else{
+        // Gece Modu
+  
+
+        tema.classList.replace("bg-light", "bg-dark");
+        icons.className = "fa-regular fa-sun";
+        buton.classList.replace("btn-dark", "btn-light");
+        content.classList.replace("text-dark", "text-light");
+       
+    }
+   
+}
